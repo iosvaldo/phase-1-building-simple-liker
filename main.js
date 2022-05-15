f364a3e348e4dba4714679ffc31cdf49a8fff3f3
osvaldo <iosvaldo.app@gmail.com>
2022-05-15 19:41:52 -0400

    commit tree 7813ff89ef155a1d7cebba6bf3ed8355921a0475

--- main.js
+++ main.js
@@ -3,34 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let likeGlyphs = document.querySelectorAll(".like-glyph");
 
-const arrayLikes = Array.from(document.querySelectorAll('ul span'));
 
-arrayLikes.forEach( (heartBtn) => {
+likeGlyphs.forEach(likeGlyph => {
+likeGlyph.addEventListener("click",invokeServerCall);
+});
 
-function like() {
+function invokeServerCall(e){
+  let likeGlyph = e.target
   mimicServerCall()
-  .then( () => {
-   if (heartBtn.innerText === EMPTY_HEART) {
-      heartBtn.innerText = FULL_HEART;
-      heartBtn.className = 'like-glyph activated-heart';
-
-   } else if (heartBtn.innerText === FULL_HEART) {
-      heartBtn.innerText = EMPTY_HEART;
-      heartBtn.className = 'like-glyph';
-      }
-    }
-  )
-  .catch(() => {
-  document.querySelector('#modal').className = ''
-
-  document.querySelector('#modal').innerText = "Random server error. Try again."
-
-  setTimeout(() => {document.querySelector('#modal').className = 'hidden'}, 3000)
-  })
-}
-heartBtn.addEventListener('click', like);
+.then((res) => {
+  // let activeHeart = document.querySelector(".activated-heart")
+  if(likeGlyph.textContent === EMPTY_HEART){
+    likeGlyph.textContent = FULL_HEART
+    likeGlyph.classList.add("activated-heart")
+  }else{
+    likeGlyph.textContent = EMPTY_HEART
+    likeGlyph.classList.remove("activated-heart")
+  }
+  console.log(res)
 })
+.catch((error) => {
+  let element = document.getElementById("modal")
+  element.classList.remove("hidden")
+//  console.log(error.message)
+  myTimeOut = setTimeout(myStopFunction, 3000);
+
+  function myStopFunction() {
+  let addElement = document.getElementById("modal")
+  console.log(addElement)
+  addElement.classList.add("hidden");
+  clearTimeout(myTimeOut);
+}
+})
+console.log("toast!")
+}
+
+
+
+
 
 
 
